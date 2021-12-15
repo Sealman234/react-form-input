3ff1bfbdde1d5bb359625e6dc4a2a56a6dc51ac5
Sealman <castle2668@gmail.com>
2021-12-15 18:56:43 +0800

    加上基本的表單送出驗證

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import React, { useState, useRef } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef(null);
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,6 +12,12 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault(); // 取消送出後的預設行為 (Send HTTP Request)
 
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
     // console.log(nameInputRef.current.value); => 如果送出的值只會在這邊使用，就用 useRef
     console.log(enteredName); // 如果需要監控輸入值的變化，像是即時驗證，則使用 useState
 
@@ -18,9 +25,13 @@ const SimpleInput = (props) => {
     setEnteredName(''); // 使用 useState 做送出後清空的功能
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? 'form-control'
+    : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -29,6 +40,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button type="submit">Submit</button>
