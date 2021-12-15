a3f8f0f5b75968fa75a95f2229bce88cdf885ff7
Sealman <castle2668@gmail.com>
2021-12-15 17:57:23 +0800

    表單資料使用 useState 與 useRef 的差別

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,37 @@
+import React, { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef(null);
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // 取消送出後的預設行為 (Send HTTP Request)
+
+    // console.log(nameInputRef.current.value); => 如果送出的值只會在這邊使用，就用 useRef
+    console.log(enteredName); // 如果需要監控輸入值的變化，像是即時驗證，則使用 useState
+
+    // nameInputRef.current.value = ''; => 不建議，不要直接更改 DOM
+    setEnteredName(''); // 使用 useState 做送出後清空的功能
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
