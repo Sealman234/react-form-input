7856630fef54bbe96e78f57b6f478378bef66b18
Sealman <castle2668@gmail.com>
2021-12-16 11:40:06 +0800

    Add more complete validation for name input field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,46 +1,46 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef(null);
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formSubmissionHandler = (event) => {
-    event.preventDefault(); // 取消送出後的預設行為 (Send HTTP Request)
-
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-      return;
-    }
-    setEnteredNameIsValid(true);
-
-    // console.log(nameInputRef.current.value); => 如果送出的值只會在這邊使用，就用 useRef
-    console.log(enteredName); // 如果需要監控輸入值的變化，像是即時驗證，則使用 useState
-
-    // nameInputRef.current.value = ''; => 不建議，不要直接更改 DOM
-    setEnteredName(''); // 使用 useState 做送出後清空的功能
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouched(true);
   };
 
-  const nameInputClasses = enteredNameIsValid
-    ? 'form-control'
-    : 'form-control invalid';
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) return;
+
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
-        {!enteredNameIsValid && (
+        {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
         )}
       </div>
